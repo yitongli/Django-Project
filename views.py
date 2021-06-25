947d24722d673d535634eb9895851ef1c2f356e2
yitongli <yitongliphysics@gmail.com>
2021-06-25 22:28:19 +0800

    Update views.py

--- views.py
+++ views.py
@@ -5,16 +5,28 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseNotFound,HttpResponseRedirect
+
+monthly_challenges = {
+    "january":"not eat meat",
+    "february":"walk 20 minutes everyday!!!"
+}
+
+# Create your views here.
 def monthly_challenges_by_numbers(request,month):
-    return HttpResponse(month)
+    months_list = list(monthly_challenges.keys())
+    if month > len(months_list):
+        return HttpResponseNotFound("this month is not supported!")
+    forward_month = months_list[month-1]
+    #redirect function, convert int month to string month
+    return HttpResponseRedirect("/challenges/"+forward_month)
 
 
 def monthly_challenge(request,month):
-    challenge_text = None
-    if month == 'january':
-        challenge_text = "not eat meat"
-    elif month == 'february':
-        challenge_text = "walk 20 minutes everyday!!!"
-    else:
-        return HttpResponseNotFound("this month is not supported")
-    return HttpResponse(challenge_text)
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("this month is not supported!")
+    
