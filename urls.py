11787f88e8571172da7ffb5349abcf398087358b
yitongli <yitongliphysics@gmail.com>
2021-06-26 15:55:50 +0800

    Update urls.py

--- urls.py
+++ urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 
+
 urlpatterns = [
+    path("",views.index),#trigger as /challenges
     path("<int:month>",views.monthly_challenges_by_numbers),
     path("<str:month>",views.monthly_challenge, name="month-challenge")
     
 ]
+
 
