2add681828b2b830fe3eea4411df3ef5c57b9283
yitongli <yitongliphysics@gmail.com>
2021-06-26 15:56:23 +0800

    Update views.py

--- views.py
+++ views.py
@@ -8,6 +8,20 @@ monthly_challenges = {
 }
 
 # Create your views here.
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge",args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+
+    #"<li><li>" create month list 
+    
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
 def monthly_challenges_by_numbers(request,month):
     months_list = list(monthly_challenges.keys())
     if month > len(months_list):
@@ -22,7 +36,8 @@ def monthly_challenges_by_numbers(request,month):
 def monthly_challenge(request,month):
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("this month is not supported!")
+        return HttpResponseNotFound("<h1>this month is not supported!</h1>")
     
